f5a3f2159d62abab8ec2976054581d42a16c6db4
Ian <ianw3214@gmail.com>
2016-05-03 23:43:01 -0700

    small fixes
    changed font for content, tweaked font sizes
    updated code for mobile scrolling
    (hopefully works now)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-// code to detect mobile
-if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-  document.getElementById("ctn").style.overflow = scroll;
+// code the detect mobile
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+// change the overflow style depending on device
+if(isMobile.any()){
+  document.getElementById("ctn").style.overflow = "scroll";
+  document.getElementById("main").height = "auto";
+  document.getElementById("content").height = "auto";
 }else{
-  document.getElementById("ctn").style.overflow = hidden;
+  document.getElementById("ctn").style.overflow = "hidden";
 }
