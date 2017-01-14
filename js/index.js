b746f325418a8d10c3b42668a50e38471a4067d6
Ian Wang <ianw3214@users.noreply.github.com>
2017-01-14 17:20:01 -0500

    fix typo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var data = {
-    "cpp":"3+ years of experience building desktop applications and games with C++. <br> Completed the National Taiwan Uniersitys' data structure and advanced C++ programming course.",
+    "cpp":"3+ years of experience building desktop applications and games with C++. <br> Completed the National Taiwan Universitys' data structure and advanced C++ programming course.",
     "python":"Experience building small scale scripts to solve problems. <br> Completed Univeristy of Toronto CSC148, Introduction to computer science",
     "js":"Experience building several small web applications that use external APIs to communicate with servers. <br> Currently working through the FreeCodeCamp web development Curriculum.",
     "css3":"Experience building several small websites with a variety of CSS frameworks.",
